8ad46ac0b7f6513f2ca59a13e355477ed40722d9
Victor H. Herrera <hugo.tmz96@gmail.com>
2021-10-30 22:04:14 -0500

    feat: Add dummy operator

--- dags/simple_dag.py
+++ dags/simple_dag.py
@@ -1,4 +1,7 @@
 from airflow import DAG
+from airflow.operators.dummy import DummyOperator
 
-with DAG(dag_id = 'simple_dag') as dag:
-    None
+from datetime import datetime
+
+with DAG(dag_id = 'simple_dag', start_date = datetime(2021, 1, 1)) as dag:
+    DummyOperator(task_id = 'task_1')
