a822f481517aa8108c25d744ee76b39c5017ce7c
Victor H. Herrera <hugo.tmz96@gmail.com>
2021-11-02 19:05:41 -0600

    feat: Add python operator

--- dags/simple_dag.py
+++ dags/simple_dag.py
@@ -1,9 +1,14 @@
 from airflow import DAG
-from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 
 from datetime import datetime, time, timedelta
 
+def _downloading_data(my_parameter):
+     print(my_parameter)
+
 with DAG(dag_id = 'simple_dag', schedule_interval = "@daily", 
          start_date = days_ago(3), catchup = False) as dag:
-    task_1 = DummyOperator(task_id = 'task_1')
+    downloading_data = PythonOperator(task_id = 'downloading_data', 
+                                      python_callable = _downloading_data,
+                                      op_kwargs = {'my_parameter': 42})
